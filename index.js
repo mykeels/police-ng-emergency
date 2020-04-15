2b41d5854324cd4423ca0fdf1f4db1a7e607227c
lilpolymath <ayofavour7@gmail.com>
2020-04-15 13:19:40 +0100

    Update Data and formatting.
    feat: add new data for Lagos state and a new resolver.

--- index.js
+++ index.js
@@ -1,33 +1,78 @@
 const fs = require('fs');
 const path = require('path');
 
-const { states } = require('./emergency-contacts.json');
+const states = require('./lagos.json');
 
-console.log(states.map(({ state }) => `- [${state.toUpperCase()}](https://raw.githubusercontent.com/mykeels/police-ng-emergency/master/vcf/${state}.vcf)`).join('\n'));
+const contacts = ({ state, phones, areas, commandControlRooms }) => {
+  const main = `BEGIN:VCARD
+BDAY;VALUE=DATE:1960-10-01
+VERSION:3.0
+N:${state};Police;NG
+FN:${state} Police NG
+ORG:Police NG
+${phones.map(phone => `TEL;TYPE=WORK,MSG:${phone}`).join('\n')}
+END:VCARD`;
+
+  const contactResolver = ({ name, phone }) =>
+    `BEGIN:VCARD
+BDAY;VALUE=DATE:1960-10-01
+VERSION:3.0
+N:${name};Police;NG
+FN:${(name, state)} Police NG
+ORG:Police NG
+TEL;TYPE=WORK,MSG:${phone}
+END:VCARD`;
+
+  const area = areas.map(area => contactResolver(area));
+
+  const divisions = areas.map(({ divisions }) =>
+    divisions.map(division => contactResolver(division))
+  );
+
+  const commandControl = commandControlRooms.map(room => contactResolver(room));
+
+  return {
+    main,
+    area,
+    divisions,
+    commandControl,
+  };
+};
+
+const final = contacts(states);
+console.log(final);
+
+// console.log(
+//   states
+//     .map(
+//       ({ state }) =>
+//         `- [${state.toUpperCase()}](https://raw.githubusercontent.com/mykeels/police-ng-emergency/master/vcf/${state}.vcf)`
+//     )
+//     .join('\n')
+// );
 
 // const contacts = states.map(({ state, phones }) => {
 //     return {
 //         state,
-//         content: `BEGIN:VCARD
+// content: `BEGIN:VCARD
 // BDAY;VALUE=DATE:1960-10-01
 // VERSION:3.0
 // N:${state};Police;NG
 // FN:${state} Police NG
 // ORG:Police NG
 // ${phones.map(phone => `TEL;TYPE=WORK,MSG:${phone}`).join('\n')}
-// END:VCARD`
+// END:VCARD`;
 //     }
 // });
 
-// for (let contact of contacts)
-// {
-//     fs.writeFileSync(path.resolve(`./${contact.state}.vcf`), contact.content, { encoding: 'utf8' });
+// for (let contact of contacts) {
+//   fs.writeFileSync(path.resolve(`./${contact.state}.vcf`), contact.content, {
+//     encoding: 'utf8',
+//   });
 // }
 
-// fs.writeFileSync(
-//     path.resolve(`./nigeria.vcf`), 
-//     contacts.map(contact => contact.content).join('\n'), 
-//     { encoding: 'utf8' }
-// );
-
-// console.log(contacts)
+fs.writeFileSync(
+  path.resolve(`./lagos-new.vcf`),
+  final.area.map(contact => contact).join('\n'),
+  { encoding: 'utf8' }
+);
